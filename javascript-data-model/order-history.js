178181c337296d595ec97b93d08774ea2e47705b
Tai Doan <DoanTai11@gmail.com>
2023-10-24 17:38:49 +0000

    Completed javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,80 @@
+const orderHistory = [
+  {
+    orderNumber: '114-3941689-8772232',
+    orderDate: 'August 4, 2020',
+    orderTotal: 34,
+    deliveryDate: 'Aug 8, 2020',
+    shipToName: 'JS Masher',
+    item: [
+      {
+        title: 'JavaScript for impatient programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        returnWindow: 'Sep 7, 2020',
+        price: 31.55,
+        itemNumber: '',
+      },
+    ],
+  },
+
+  {
+    orderNumber: '113-9984268-1280257',
+    orderDate: 'July 19, 2020',
+    orderTotal: 44.53,
+    deliveryDate: 'Jul 20, 2020',
+    shipToName: 'JS Masher',
+    item: [
+      {
+        title: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        returnWindow: 'Aug 19, 2020',
+        price: 41.33,
+        itemNumber: '',
+      },
+    ],
+  },
+
+  {
+    orderNumber: '114-2875557-9059409',
+    orderDate: 'July 4, 2020',
+    orderTotal: 17.22,
+    deliveryDate: 'Jul 7, 2020',
+    shipToName: 'JS Masher',
+    item: [
+      {
+        title:
+          'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for Wii U, PC. Support Turbo and Vibration Feartures. No Driver and No Lag-Gamecube Adapter',
+        author: '',
+        returnWindow: 'Aug 5, 2020',
+        price: 15.98,
+        itemNumber: '',
+      },
+    ],
+  },
+
+  {
+    orderNumber: '113-2883177-2648248',
+    orderDate: 'July 3, 2020',
+    orderTotal: 138.93,
+    deliveryDate: 'Jul 5, 2020',
+    shipToName: 'JS Masher',
+    item: [
+      {
+        title:
+          'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        author: '',
+        returnWindow: 'Aug 4, 2020',
+        price: 94.95,
+        itemNumber: '',
+      },
+      {
+        title: 'The Art of Sql',
+        author: 'Faroult, Stephane',
+        returnWindow: 'Aug 4, 2020',
+        price: 33.99,
+        itemNumber: '',
+      },
+    ],
+  },
+];
+
+console.log(orderHistory);
