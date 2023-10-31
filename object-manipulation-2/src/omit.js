eadf9aaaf06335559bf38ff00a58d1c9b6d06ad9
Tai Doan <DoanTai11@gmail.com>
2023-10-31 05:56:11 +0000

    work in progress

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,14 @@
 /* exported omit */
+function omit(source, keys) {
+  const object = {};
+  for (const prop of keys) {
+    const key = source[prop];
+    if (key !== undefined) {
+      object[prop] = source[prop];
+    }
+  }
+  return object;
+
+  /* use a for-in loop for source[key]
+  conditional using array(keys).includes(key) */
+}
