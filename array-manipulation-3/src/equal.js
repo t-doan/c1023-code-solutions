cd6399437e479acebf5e676b128d801da0ab6153
Tai Doan <DoanTai11@gmail.com>
2023-10-31 17:32:32 +0000

    Work in progress

--- array-manipulation-3/src/equal.js
+++ array-manipulation-3/src/equal.js
@@ -1,1 +1,20 @@
 /* exported equal */
+function equal(first, second) {
+  let boolean;
+
+  if (first.length === second.length) {
+    for (let i = 0; i < first.length; i++) {
+      const firstInt = first[i];
+      const secondInt = second[i];
+      if (firstInt === secondInt) {
+        boolean = true;
+      } else {
+        boolean = false;
+        break;
+      }
+    }
+  } else {
+    boolean = false;
+  }
+  return boolean;
+}
