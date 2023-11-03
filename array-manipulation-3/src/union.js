b5a4c14032da4704f11680da08b85dd500c19838
Tai Doan <DoanTai11@gmail.com>
2023-11-03 22:17:24 +0000

    completed array manipulation 3

--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,2 +1,17 @@
 /* exported union */
-function union(first, second) {}
+function union(first, second) {
+  const uniqueArray = [];
+  let value;
+  for (let i = 0; i < first.length; i++) {
+    value = first[i];
+    uniqueArray.push(first[i]);
+  }
+
+  for (let i = 0; i < second.length; i++) {
+    value = second[i];
+    if (!first.includes(value)) {
+      uniqueArray.push(second[i]);
+    }
+  }
+  return uniqueArray;
+}
