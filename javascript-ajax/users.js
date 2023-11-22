3a8c0e906fc7482ff4625f50bb9751625b0a18a5
Tai Doan <DoanTai11@gmail.com>
2023-11-22 18:07:49 +0000

    completed javascript-ajax

---
+++ javascript-ajax/users.js
@@ -0,0 +1,15 @@
+const $userList = document.querySelector('#user-list');
+
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+xhr.responseType = 'json';
+xhr.addEventListener('load', function () {
+  console.log(xhr.status);
+  console.log(xhr.response);
+  for (const user of xhr.response) {
+    const newLi = document.createElement('li');
+    newLi.textContent = user.name;
+    $userList.appendChild(newLi);
+  }
+});
+xhr.send();
