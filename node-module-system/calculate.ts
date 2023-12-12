c0c2294423a7c7d7c7b56967475285b9fa83c913
Tai Doan <DoanTai11@gmail.com>
2023-12-12 20:15:40 +0000

    fix error

--- node-module-system/calculate.ts
+++ node-module-system/calculate.ts
@@ -4,19 +4,35 @@ import { multiply } from './multiply.js';
 import { divide } from './divide.js';
 
 const x: number = +process.argv[2];
+const operator: string = process.argv[3];
 const y: number = +process.argv[4];
 let result: number;
 
-if (process.argv[3] === 'plus') {
-  result = add(x, y);
-  console.log('result:', result);
-} else if (process.argv[3] === 'minus') {
-  result = subtract(x, y);
-  console.log('result:', result);
-} else if (process.argv[3] === 'times') {
-  result = multiply(x, y);
-  console.log('result:', result);
-} else if (process.argv[3] === 'over') {
-  result = divide(x, y);
-  console.log('result:', result);
+if (Number.isNaN(x)) {
+  console.log(`${process.argv[2]} is not a number`);
+  process.exit(1);
 }
+if (Number.isNaN(y)) {
+  console.log(`${process.argv[4]} is not a number`);
+  process.exit(1);
+}
+
+switch (operator) {
+  case 'plus':
+    result = add(x, y);
+    break;
+  case 'minus':
+    result = subtract(x, y);
+    break;
+  case 'times':
+    result = multiply(x, y);
+    break;
+  case 'over':
+    result = divide(x, y);
+    break;
+  default:
+    console.log('invalid operator');
+    process.exit(1);
+}
+
+console.log('result:', result);
