7df302dac75a37f3b24d93d54af7baa29665965c
Tai Doan <DoanTai11@gmail.com>
2023-12-15 23:46:07 +0000

    completed express-post-json

---
+++ express-post-json/server.ts
@@ -0,0 +1,44 @@
+import express from 'express';
+
+const app = express();
+
+let nextId = 1;
+
+type Grade = {
+  id: number;
+  name: string;
+  course: string;
+  score: number;
+};
+
+const grades: Record<number, Grade> = {};
+
+app.use(express.json());
+
+app.get('/api/grades', (req, res) => {
+  const gradesArray: Grade[] = [];
+  for (const x in grades) {
+    gradesArray.push(grades[x]);
+  }
+  res.json(gradesArray);
+});
+
+app.post('/api/grades', (req, res) => {
+  if (!req.body.name || !req.body.course || !req.body.score) {
+    res.sendStatus(404);
+  } else {
+    const newGrade: Grade = {
+      id: nextId++,
+      name: req.body.name,
+      course: req.body.course,
+      score: req.body.score,
+    };
+
+    grades[newGrade.id] = newGrade;
+    res.status(201).json(newGrade);
+  }
+});
+
+app.listen(8080, () => {
+  console.log('Express server listening on port 8080');
+});
